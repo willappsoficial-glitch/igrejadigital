01f14f979b1d83bb5854a579852ee9e7295dde1c
willappsoficial-glitch <willappsoficial@gmail.com>
2026-07-22 13:25:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = ''; 
+const API_URL = 'https://script.google.com/macros/s/AKfycbwowZOp5zrgL7pqtrKkCQJDX7k8EkEoqdjfni-6v8gTV2ZIyL4NXA8O7jSz7PdgKNmc/exec'; 
 
 window.onload = function() {
     initTheme();
